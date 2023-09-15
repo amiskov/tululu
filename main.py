5e7762b75fd7dd33ed689ad3fd01f05c62a48324
Andrey Miskov <amiskov@gmail.com>
2023-09-15 16:31:25 +0300

    download books securely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import logging
+from pathvalidate import sanitize_filename
 
 import requests
 
@@ -7,24 +8,54 @@ import requests
 def main():
     for book_id in range(1, 11):
         url = f'https://tululu.org/txt.php?id={book_id}'
-        books_dir = Path('books')
-        books_dir.mkdir(exist_ok=True)
-        book_name = f'id{book_id}.txt'
-        download_book(books_dir, url, book_name)
+        try:
+            download_txt(url, 'books', book_id)
+        except requests.HTTPError as e:
+            logging.error(e)
+            continue
 
 
-def download_book(dir: Path, url: str, book_name: str) -> Path:
-    """Download book by `url` to `dir`.
+def check_for_redirect(resp: requests.Response):
+    if resp.history and 'txt.php?id=' not in resp.url:
+        raise requests.HTTPError('Bad book URL.')
 
-    `url` must have a file extension at the end (`.png`, `.jpg`, etc).
+
+def download_txt(url: str, filename: str, folder='books/') -> str:
+    """Функция для скачивания текстовых файлов.
+
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+
+    Examples:
+        url = 'http://tululu.org/txt.php?id=1'
+
+        filepath = download_txt(url, 'Алиби')
+        print(filepath)  # Выведется books/Алиби.txt
+
+        filepath = download_txt(url, 'Али/би', folder='books/')
+        print(filepath)  # Выведется books/Алиби.txt
+
+        filepath = download_txt(url, 'Али\\би', folder='txt/')
+        print(filepath)  # Выведется txt/Алиби.txt
     """
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
 
-    with open(dir.joinpath(book_name), 'wb') as file:
+    books_dir = Path(folder)
+    books_dir.mkdir(exist_ok=True)
+
+    book_file = books_dir.joinpath(sanitize_filename(filename + '.txt'))
+
+    with open(book_file, 'wb') as file:
         file.write(response.content)
-    logging.info(f'{book_name} has been saved.')
-    return dir / book_name 
+    logging.info(f'{filename} has been saved.')
+    return str(book_file)
 
 
 if __name__ == '__main__':
