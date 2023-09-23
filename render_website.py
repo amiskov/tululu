7cafc153c8d0735242646f6263620866f61d4055
Andrey Miskov <amiskov@gmail.com>
2023-09-23 15:21:17 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,3 +1,5 @@
+import os
+import math
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -5,28 +7,35 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def on_reload():
-    books = []
-    with open('books.json', 'r') as my_file:
-        books = json.loads(my_file.read())
+def on_reload(db_filename: str, target_dir: str):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
 
-    rendered_page = template.render(
-        books=chunked(books, 2),
-    )
+    with open(db_filename, 'r') as books_db_file:
+        books_db = books_db_file.read()
+    books = json.loads(books_db)
+    total_pages = math.ceil(len(books) / 10)
+    print(total_pages, )
+    books_pages = chunked(books, 10)
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for (page_num, books) in enumerate(books_pages, start=1):
+        rendered_page = template.render(
+            books=chunked(books, 2),
+            total_pages=total_pages
+        )
+        with open(f'{target_dir}/index{page_num}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
+    target_dir = 'pages'
+    os.makedirs(target_dir, exist_ok=True)
     server = Server()
-    on_reload()
-    server.watch('template.html', on_reload, delay='forever')
+    on_reload('books.json', target_dir)
+    server.watch('template.html', lambda: on_reload('books.json', target_dir), delay='forever')
     server.serve(root='.')
 
 
